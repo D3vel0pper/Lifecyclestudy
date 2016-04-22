5e2890e1e8a587c1faed9338f918208835b9005f
D3vel0pper <toamaindex@gmail.com>
2016-04-22 17:41:33 +0900

    add the ND Activity

--- app/src/main/java/t4ka/com/lifecyclestudy/activity/MainActivity.java
+++ app/src/main/java/t4ka/com/lifecyclestudy/activity/MainActivity.java
@@ -10,6 +10,8 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -26,47 +28,14 @@ import java.util.List;
 import t4ka.com.lifecyclestudy.R;
 import t4ka.com.lifecyclestudy.Service.MyService;
 
-/**
- * At first, You Must Check the AndroidManifest
- * Then, check is it contained service which you want to use
- * If there isn't, you must add the tag <service></service>
- */
-
 public class MainActivity extends Activity implements OnClickListener{
 
-    /**
-     * myService -> holder for holding MyService
-     * boolean bound -> flag of that is bound or not
-     */
-    MyService myService;
-    boolean bound = false;
 
     /**
-     * call when connect to or disconnect from the service. This is passed to bindService()
+     * I added
+     *      compile 'com.android.support:support-v4:22.0.0
+     * in app/build.gradle
      */
-    private ServiceConnection serviceConnection = new ServiceConnection() {
-
-        /**
-         * Called when connect to service
-         */
-        @Override
-        public void onServiceConnected(ComponentName name, IBinder service) {
-            MyService.MyServiceLocalBinder localBinder = (MyService.MyServiceLocalBinder)service;
-            myService = localBinder.getService();
-            Toast.makeText(MainActivity.this,"onServiceConnected()",Toast.LENGTH_SHORT).show();
-            bound = true;
-        }
-
-        /**
-         * Called when disconnect from service
-         */
-        @Override
-        public void onServiceDisconnected(ComponentName name) {
-            Toast.makeText(MainActivity.this,"onServiceDisconnect()",Toast.LENGTH_SHORT).show();
-            bound = false;
-        }
-    };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,12 +44,6 @@ public class MainActivity extends Activity implements OnClickListener{
 
         Button StartBtn = (Button)findViewById(R.id.StartBtn);
         StartBtn.setOnClickListener(this);
-        Button StopBtn = (Button)findViewById(R.id.StopBtn);
-        StopBtn.setOnClickListener(this);
-        Button BindBtn = (Button)findViewById(R.id.BindBtn);
-        BindBtn.setOnClickListener(this);
-        Button UnBindBtn = (Button)findViewById(R.id.UnBindBtn);
-        UnBindBtn.setOnClickListener(this);
 
     }
 
@@ -119,36 +82,18 @@ public class MainActivity extends Activity implements OnClickListener{
         Toast.makeText(this,"onRestart called",Toast.LENGTH_SHORT).show();
     }
 
-    /**
-     * without stopping description, Resource will be leak and throw Exception
-     */
     @Override
     public void onDestroy(){
         super.onDestroy();
-        stopService(new Intent(MainActivity.this,MyService.class));
-        if(bound){
-            unbindService(serviceConnection);
-            bound = false;
-        }
         Toast.makeText(this,"onDestroy called",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     public void onClick(View v){
         if(v.getId() == R.id.StartBtn){
-            startService(new Intent(MainActivity.this,MyService.class));
-        } else if(v.getId() == R.id.StopBtn){
-            stopService(new Intent(MainActivity.this,MyService.class));
-        } else if(v.getId() == R.id.BindBtn){
-            Intent intent = new Intent(MainActivity.this,MyService.class);
-            bindService(intent,serviceConnection, Context.BIND_AUTO_CREATE );
-        } else if(v.getId() == R.id.UnBindBtn){
-            if(bound){
-                unbindService(serviceConnection);
-                bound = false;
-            }
+            Intent intent = new Intent(MainActivity.this,DrawerActivity.class);
+            startActivity(intent);
         }
-
     }
 
 
